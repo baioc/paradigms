543f66b817824469e5a858fe355a109140891fe4
Gabriel B SantAnna <baiocchi.gabriel@gmail.com>
2019-04-30 16:14:38 -0300

    Rename assembly mutex functions

--- Assembly/atomic.asm
+++ Assembly/atomic.asm
@@ -1,20 +1,20 @@
 .text
 
 
-# void lock(semaphore)
+# void lock(mutex)
 lock:
-	ll $t0, 0($a0)	# read the semaphore (and set LLbit)
+	ll $t0, 0($a0)	# read the mutex (and set LLbit)
 	bne $t0, $zero, lock	# if occupied (1), start over
 
 	addi $t1, $zero, 1
-	sc $t1, 0($a0)	# try grabing the semaphore
+	sc $t1, 0($a0)	# try grabing the mutex
 	beq $t1, $zero, lock	# if wasn't atomic (0), start over
 
-	# return only when sucessfully grabbed the semaphore in an atomic swap
+	# return only when sucessfully grabbed the mutex in an atomic swap
 	jr $ra
 
 
-# void unlock(semaphore)
+# void unlock(mutex)
 unlock:
 	sw $zero, 0($a0)
 	jr $ra
