85a5e2b6411e90306387d896cca08803143f6ebc
Gabriel B SantAnna <baiocchi.gabriel@gmail.com>
2019-05-07 16:28:44 -0300

    Add Interpolation Search

--- C/generic/generic.c
+++ C/generic/generic.c
@@ -34,7 +34,7 @@ void *my_bsearch(const void *key, const void *base, int length,
 	while (low <= high) {
 		mid = (low + high) / 2;
 		void *addr = (char*) base + mid*type_size;
-		int diff = cmpfn(key, addr);
+		const int diff = cmpfn(key, addr);
 		if (diff == 0)
 			return addr;
 		else if (diff > 0)
@@ -52,7 +52,7 @@ void *my_bsearch(const void *key, const void *base, int length,
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
-void test_swap(void)
+static void test_swap(void)
 {
 	int x = 5, y = 7;
 	swap(&x, &y, sizeof(x));
@@ -66,26 +66,51 @@ void test_swap(void)
 	#endif // DEBUG
 }
 
-int intcmp(const void *i1, const void *i2)
+static int intcmp(const void *i1, const void *i2)
 {
 	const int *p1 = (int *)i1;
 	const int *p2 = (int *)i2;
 	return *p1 - *p2;
 }
 
-int my_strcmp(const void *s1, const void *s2)
+static int my_strcmp(const void *s1, const void *s2)
 {
 	const char *c1 = *(char **)s1;
 	const char *c2 = *(char **)s2;
 	return strcmp(c1, c2);
 }
 
-void test_search(void)
+int lerp(int x, int in_min, int in_max, int out_min, int out_max)
+{
+	return out_min + ((x - in_min) * (out_max - out_min)) / (in_max - in_min);
+}
+
+int lerpsearch(int target, const int *array, int length)
+{
+	int low = 0;
+	int high = length - 1;
+	int lerped;
+
+	while (low <= high) {
+		lerped = lerp(target, array[low], array[high], low, high);
+		const int found = array[lerped];
+		if (found == target)
+			return lerped;
+		else if (target > found)
+			low = lerped + 1;
+		else
+			high = lerped - 1;
+	}
+
+	return -1;
+}
+
+static void test_search(void)
 {
 	{
-		int array[] = {-6, 0, 2, 3, 6, 7, 11};
-		int number = 6;
-		int *found = my_bsearch(&number, array, ARRAY_SIZE(array),
+		const int array[] = {-6, 0, 2, 3, 6, 7, 11};
+		const int number = 6;
+		const int *found = my_bsearch(&number, array, ARRAY_SIZE(array),
 		                        sizeof(number), intcmp);
 		assert(found != NULL);
 		#if DEBUG
@@ -94,9 +119,18 @@ void test_search(void)
 	}
 
 	{
+		const int numbers[] = {-6, 0, 2, 3, 6, 7, 11};
+		const int number = 6;
+		const int found = lerpsearch(number, numbers, ARRAY_SIZE(numbers));
+		#if DEBUG
+			printf("Found: %d\n", numbers[found]);
+		#endif // DEBUG
+	}
+
+	{
 		const char *notes[] = {"Ab", "B", "D", "F#", "Gb"};
 		const char *fav_note = "B";
-		char **found = lsearch(&fav_note, notes, ARRAY_SIZE(notes),
+		const char **found = lsearch(&fav_note, notes, ARRAY_SIZE(notes),
 							   sizeof(fav_note), my_strcmp);
 		assert(found != NULL);
 		#if DEBUG
@@ -105,7 +139,7 @@ void test_search(void)
 	}
 }
 
-int main(int argc, char const *argv[])
+int main(int argc, const char *argv[])
 {
 	test_swap();
 	test_search();
