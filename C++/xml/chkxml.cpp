5612744395c1f4ac4630f8e7d483a34c08259d69
Gabriel B SantAnna <baiocchi.gabriel@gmail.com>
2019-05-17 20:26:00 -0300

    Implement stdin usage by chkxml

--- C++/xml/chkxml.cpp
+++ C++/xml/chkxml.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <fstream>	// filestream
-#include <sstream>	// stringstream
-#include <vector>
+#include <fstream>  // filestream
+#include <sstream>  // stringstream
+#include <stack>
 #include <string>
+#include <string.h>
 
 
 bool balanced(const std::string& xml);
@@ -10,26 +11,41 @@ bool balanced(const std::string& xml);
 int main(int argc, char const *argv[])
 {
 	if (argc < 2) {
-		std::cout << "Usage: " << argv[0] << " [file.xml]...\n";
+		std::cout << "chkxml:\n"
+		          << "  Checks if given XMLs' tag structures are valid.\n"
+		          << "  Returns the number of files found to be incorrect, echo $? to see this value.\n"
+		          << "\nUsage:\n"
+		          << "  " << argv[0] << " file.xml...\n"
+		          << "  " << argv[0] << " < file.xml\n"
+		          << "  cat file.xml | " << argv[0] << " - [file.xml...]\n";
 		return -1;
 	}
 
 	int err = 0;
 	for (int i = 1; i < argc; ++i) {
-		std::ifstream xml(argv[i]);
-		if (!xml.is_open()) {
-			std::cout << "Could not open " << argv[i] << '\n';
-			++err;
-			continue;
-		}
-
 		std::stringstream buffer;
-		buffer << xml.rdbuf();
-		xml.close();
+
+		// if actual filename
+		if (strcmp(argv[i], "-") != 0) {
+			std::ifstream file{argv[i]};
+
+			if (!file.is_open()) {
+				std::cout << "Could not open " << argv[i] << '\n';
+				++err;
+				continue;
+			}
+
+			buffer << file.rdbuf();
+			file.close();
+		}
+		// otherwise use stdin
+		else {
+			buffer << std::cin.rdbuf();
+		}
 
 		const bool ok = balanced(buffer.str());
 		err += !ok;
-		std::cout << argv[i]
+		std::cout << (strcmp(argv[i], "-") != 0 ? argv[i] : "Input")
 		          << (ok ? " seems OK." : " is NOT correct.")
 		          << '\n';
 	}
@@ -39,7 +55,7 @@ int main(int argc, char const *argv[])
 
 bool balanced(const std::string& xml)
 {
-	std::vector<std::string> tags;
+	std::stack<std::string> tags;
 
 	auto from = 0u;
 	while (from < xml.length()) {
@@ -61,13 +77,14 @@ bool balanced(const std::string& xml)
 
 		// if its an opening tag, push the closing one
 		if (tag[1] != '/') {
-			tags.push_back(tag.insert(1, "/"));
-		} // if its a closing tag, check if it was expected
+			tags.push(tag.insert(1, "/"));
+		}
+		// otherwise (if its a closing tag), check if it was expected
 		else {
 			if (tags.empty())
 				return false;
-			else if (tags.back() == tag)
-				tags.pop_back();
+			else if (tags.top() == tag)
+				tags.pop();
 			else
 				return false;
 		}
