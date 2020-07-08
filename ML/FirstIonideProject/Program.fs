b894f4ea825e23371ba69ec80f53268a9d713893
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2020-07-08 19:47:24 -0300

    First F# project tutorial

---
+++ ML/FirstIonideProject/Program.fs
@@ -0,0 +1,26 @@
+﻿#light "off"
+
+open System
+
+module PigLatin = begin
+    let toPigLatin (word: string) =
+        let isVowel (c: char) =
+            match c with
+            | 'a' | 'e' | 'i' |'o' |'u'
+            | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+            |_ -> false
+        in
+        if isVowel word.[0] then
+            word + "yay"
+        else
+            word.[1..] + string(word.[0]) + "ay";;
+end
+
+[<EntryPoint>]
+let main argv = begin
+    for name in argv do
+        let newName = PigLatin.toPigLatin name in
+        printfn "%s in Pig Latin is: %s" name newName;
+    done;
+    0
+end
