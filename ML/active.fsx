d0877a9d115c88a4b4523af74adf67ec28e1b07f
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2020-07-11 02:33:29 -0300

    'Filtrage de motif' (pattern matching) in F#

---
+++ ML/active.fsx
@@ -0,0 +1,34 @@
+open System.Drawing
+
+
+let (|Even|Odd|) x = if x % 2 = 0 then Even else Odd
+
+let testNumber x =
+    match x with
+    | Even -> printfn "%d is even" x
+    | Odd -> printfn "%d is odd" x
+
+
+let (|RGB|) (col : System.Drawing.Color) =
+     ( col.R, col.G, col.B )
+
+let (|HSB|) (col : System.Drawing.Color) =
+   ( col.GetHue(), col.GetSaturation(), col.GetBrightness() )
+
+let printRGB = function
+   RGB(r, g, b) -> printfn " Red: %d Green: %d Blue: %d" r g b
+
+let printHSB = function
+   HSB(h, s, b) -> printfn " Hue: %f Saturation: %f Brightness: %f" h s b
+
+let printAll col colorString =
+  printfn "%s" colorString
+  printRGB col
+  printHSB col
+
+
+printAll Color.Red "Red"
+printAll Color.Black "Black"
+printAll Color.White "White"
+printAll Color.Gray "Gray"
+printAll Color.BlanchedAlmond "BlanchedAlmond"
---
+++ ML/dawoc.fsx
@@ -0,0 +1,62 @@
+#light "off"
+
+
+let isImplication p q =
+    match p, q with
+    | true, false -> false
+    | _ -> true;;
+
+let minRational a b =
+    match a, b with
+    | ((0,_), b) -> b
+    | (a, (0,_)) -> a
+    | (((n1,d1) as r1), ((n2,d2) as r2)) -> if n1 * d2 < n2 * d1 then r1 else r2;;
+
+let charDiscriminate c =
+    match c with
+    | 'a' | 'e' | 'i' | 'o' | 'u' | 'y'
+    | 'A' | 'E' | 'I' | 'O' | 'U' | 'Y'  -> "Voyelle"
+    | c when 'a' <= c && c < 'z' || 'A' <= c && c < 'Z' -> "Consonne"
+    | c when '0' <= c && c <= '9' -> "Chiffre"
+    | _ -> "Autre";;
+
+(* `function` is short for a lambda that pattern matches on a single argument *)
+let rec foldLeft f acc = function
+    | [] -> acc
+    | elem :: list -> foldLeft f (f acc elem) list;;
+
+
+type ('a, 'b) Duple = 'a * 'b;;
+
+type Complex =
+    { Real: float;
+      Imag: float; }
+    with
+        static member (+) (z1: Complex, z2: Complex) =
+            { Real = z1.Real + z2.Real; Imag = z1.Imag + z2.Imag };
+
+        static member (*) (z1: Complex, z2: Complex) =
+            match z1, z2 with { Real = x1; Imag = y1 }, { Real = x2; Imag = y2 } ->
+                { Real = x1 * x2 - y1 * y2; Imag = x1 * y2 + x2 * y1 };
+
+        override this.ToString() =
+          if this.Imag = 0.0
+          then this.Real.ToString()
+          else this.Real.ToString() + "+" + this.Imag.ToString() + "i"
+
+        static member zero = { Real = 0.0; Imag = 0.0 };
+        static member i = { Complex.zero with Imag = 1.0 };
+    end
+
+type 't Sexp =
+    | Nil
+    | Atom of 't
+    | Pair of 't Sexp * 't Sexp;;
+
+let example = Pair(Atom("+"), Pair(Atom("1"), Pair(Atom("1"), Nil)));;
+
+let rec string sexp =
+    match sexp with
+    | Nil -> "()"
+    | Atom(symbol) -> symbol // implies 't == string
+    | Pair(car, cdr) -> "(" + (string car) + " . " + (string cdr) + ")";;
